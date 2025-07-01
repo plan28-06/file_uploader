707ec3dac1722a835ec5e149e776895ec58e8251
plan28-06 <planar2006@gmail.com>
2025-07-01 10:06:01 +0530

    Done with the header partial.

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.listen(3000, () => console.log("app listening on port 3000!"));
